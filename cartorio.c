a9c764bf36f04f88a2e901ba6ca0f2441ffd048a
Caio Taques <119276487+caiotaques@users.noreply.github.com>
2023-09-27 23:10:01 -0300

    Opção de saída
    Incluso no sistema a opção de saída.

--- cartorio.c
+++ cartorio.c
@@ -118,10 +118,11 @@ int main()
 		setlocale(LC_ALL, "Portuguese");
 		
 		printf("### Cartório da EBAC ### \n\n");
-		printf("Escolha a opção desejada do menu:\n");
+		printf("Escolha a opção desejada do menu:\n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n");
+		printf("\t3 - Deletar nomes\n\n");
+		printf("\t4 - Sair do sistema\n\t");
 		
 		scanf("%d", &opcao); //%d = inteiro
 		system("cls");
@@ -139,6 +140,10 @@ int main()
 			case 3: 
 					deletar();
 			break;
+			
+			case 4:
+				return 0;
+				break;
 			
 			default: 
 					printf("opcao invalida!\n");
